cdea40cc8b0b6837fc27ffdd736265e6142d8b0b
Armando <armandoabreu.filho@hotmail.com>
2022-06-14 12:34:31 -0300

    Inicializacao dos barcos e switch de deteccao e comeco de tirar hp

--- public/cliente.js
+++ public/cliente.js
@@ -5,6 +5,21 @@ let username; // nome do usuário
 let foe_username; // nome do oponente
 let myMatch; //id da partida do usuário
 let myId; //id do usuario
+//inicializar barcos
+var B1 = [`1`,`1`,`1`,`1`,`1`]
+var B2 = [`1`,`1`,`1`,`1`]
+var B3 = [`1`,`1`,`1`,`1`]
+var B4 = [`1`,`1`,`1`]
+var B5 = [`1`,`1`,`1`]
+var B6 = [`1`,`1`,`1`]
+var B7 = [`1`,`1`]
+var B8 = [`1`,`1`]
+var B9 = [`1`,`1`]
+var B10 = [`1`]
+var B11 = [`1`]
+var B12 = [`1`]
+var B13 = [`1`]
+//fim dos barcos
 
 ws.onmessage = (event) => {        
     console.log(event.data);
@@ -136,6 +151,74 @@ function shoot(x, y){
         posX: x,
         posY: y
     }));
+}
+
+function verifyPos(targerS)              //Verificar oq acerto prov vai ficar dentro da func shoot(tbm podia
+{                                        //colocar markboard dentro dela pra personalizar a cor dos locais
+    switch(targerS)                      //por ex: vermelho-acerto / azul mais forte-agua)
+    {
+        case`0`:
+        alert(`Agua`)
+        return `A`
+        case`1`:
+        alert(`BOOM!`)
+        //reduceHP(1)
+        return `X`
+        case`2`:
+        alert(`BOOM!`)
+        //reduceHP(2)
+        return `X`
+        case`3`:
+        alert(`BOOM!`)
+        //reduceHP(3)
+        return `X`
+        case`4`:
+        alert(`BOOM!`)
+        //reduceHP(4)
+        return `X`
+        case`5`:
+        alert(`BOOM!`)
+        //reduceHP(5)
+        return `X`
+        case`6`:
+        alert(`BOOM!`)
+        //reduceHP(6)
+        return `X`
+        case`7`:
+        alert(`BOOM!`)
+        //reduceHP(7)
+        return `X`
+        case`8`:
+        alert(`BOOM!`)
+        //reduceHP8)
+        return `X`
+        case`9`:
+        alert(`BOOM!`)
+        //reduceHP(9)
+        return `X`
+        case`10`:
+        alert(`BOOM!`)
+        //reduceHP(10)
+        return `X`
+        case`11`:
+        alert(`BOOM!`)
+        //reduceHP(11)
+        return `X`
+        case`12`:
+        alert(`BOOM!`)
+        //reduceHP(12)
+        return `X`
+        case`13`:
+        alert(`BOOM!`)
+        //reduceHP(13)
+        return `X`
+        //ADICIONAR CASO DE ACERTAR LOCAL JA MARCADO COM X OU A? OU BLOQUEAR DISSO ACONTECER?
+    }
+}
+
+function reduceHP(shipID)  //se iniciar todos os barcos como B+numero talvez de pra evitar outro switch
+{
+//  var bAcertado=B+shipID
 }
 
 function markBoard(x, y, typeCell){
